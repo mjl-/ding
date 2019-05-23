b0fd0bd887d3c661dbc8654b792a5f47561daaef
Mechiel Lukkien <mechiel@ueber.net>
2019-05-23 15:44:59 +0200

    round test coverage to whole percents

--- www-src/js/directives/coverage.js
+++ www-src/js/directives/coverage.js
@@ -6,7 +6,7 @@ app
 .directive('coverage', function() {
 	return {
 		restrict: 'E',
-		template: '<a ng-if="build.finish && build.status === &quot;success&quot;" ng-href="{{ build.coverage_report_file ? &quot;/dl/file/&quot; + repo.name + &quot;/&quot; + build.id + &quot;/&quot; + build.coverage_report_file : &quot;&quot;}}"><span ng-if="build.coverage === null" class="label label-danger"><i class="fa fa-file-code-o" ng-if="build.coverage_report_file"></i> none</span><span ng-if="build.coverage !== null" class="label" ng-class="{&quot;label-warning&quot;: build.coverage &lt; 50, &quot;label-info&quot;: build.coverage &gt;= 50 &amp;&amp; build.coverage &lt; 90, &quot;label-success&quot;: build.coverage &gt;= 90}"><i class="fa fa-file-code-o" ng-if="build.coverage_report_file"></i> {{ build.coverage }}% </span></a>',
+		template: '<a ng-if="build.finish && build.status === &quot;success&quot;" ng-href="{{ build.coverage_report_file ? &quot;/dl/file/&quot; + repo.name + &quot;/&quot; + build.id + &quot;/&quot; + build.coverage_report_file : &quot;&quot;}}"><span ng-if="build.coverage === null" class="label label-danger"><i class="fa fa-file-code-o" ng-if="build.coverage_report_file"></i> none</span><span ng-if="build.coverage !== null" class="label" ng-class="{&quot;label-warning&quot;: build.coverage &lt; 50, &quot;label-info&quot;: build.coverage &gt;= 50 &amp;&amp; build.coverage &lt; 90, &quot;label-success&quot;: build.coverage &gt;= 90}"><i class="fa fa-file-code-o" ng-if="build.coverage_report_file"></i> {{ build.coverage.toFixed(0) }}% </span></a>',
 		scope: {
 			'build': '=',
 			'repo': '='
